840c174ad9baa0a10bdba07d10cd6e271010d9ac
Mahdi_Dehghani <Mahdi14dehghani@gmail.com>
2025-03-24 01:28:02 +0330

    add 404 page

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const setStatics = require("./utils/statics");
 const app = express();
 const adminRoutes = require("./routes/admin");
 const { indexRouter } = require("./routes");
+const { get404 } = require("./controllers/404controller");
 
 //EJS
 app.set("view engine", "ejs");
 app.set("views", "views");
 //endEJS
+
 //Static Files
 setStatics(app);
 //endStatic Files
@@ -18,5 +20,9 @@ app.use(bodyParser.urlencoded());
 app.use(indexRouter);
 app.use("/admin", adminRoutes);
 //endMiddlewares
+
+//404
+app.use(get404);
+//end404
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
